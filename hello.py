ca7276b87788401bda0b6a98c719a260b886a5f9
Juan <juanito2110>
2025-02-03 19:27:07 -0600

    Changes to hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 def main():
-    print("Hello from ci-cd!")
-
+    name = input("What is your name? ")
+    age = int(input("How old are you? "))
+    year = (2023 - age) + 100
+    print(f"Hello, {name}! You will turn 100 years old in the year {year}.")
 
 if __name__ == "__main__":
     main()
